c699136cf1c3bb1825d74585d7f0a3f5553f1cee
JMiguelGlez <143457127+JMiguelGlez@users.noreply.github.com>
2023-08-29 14:31:52 -0600

    Update newClass.java

--- newClass.java
+++ newClass.java
@@ -3,6 +3,7 @@ public class newClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Se crea la nueva clase desde gitHub");
+			System.out.println("Se agrega segunda clase desde gitHub");
 			
 	}
 
