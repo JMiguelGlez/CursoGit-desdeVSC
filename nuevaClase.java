d8a109c429343d7bde0a44af719419a7e2c8628a
Miguel <klausmein@hotmail.com>
2023-08-29 14:11:22 -0600

    Comienzo en proyecto 3

---
+++ nuevaClase.java
@@ -0,0 +1,13 @@
+public class nuevaClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Pruebas en VSC");
+			
+			int test = 5;
+			System.out.println(test);
+			System.out.println("Primer parrafo agreado");
+			
+	}
+
+}
